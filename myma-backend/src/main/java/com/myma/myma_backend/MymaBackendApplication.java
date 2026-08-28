b2e9094c1d5d745d391b59a66819ac2bc82dc694
Abiisugesh <abiisugesh@gmail.com>
2026-08-28 19:51:33 +0530

    File Setup

---
+++ myma-backend/src/main/java/com/myma/myma_backend/MymaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.myma.myma_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymaBackendApplication.class, args);
+	}
+
+}
---
+++ myma-backend/src/test/java/com/myma/myma_backend/MymaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myma.myma_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
